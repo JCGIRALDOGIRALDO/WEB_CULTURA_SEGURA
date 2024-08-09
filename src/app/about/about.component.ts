35320f9d36beffe5f236fc8d03800fb867b70aad
JCGIRALDOGIRALDO <jcgiraldogiraldo19@gmail.com>
2024-08-09 09:20:27 -0500

    Empece a crear la parte del about, monte la imagen principal y estoy empezando a darle estilos a los valores

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -2,8 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-about',
-  standalone: true,
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss'],
 })
-export class AboutComponent {}
+export class AboutComponent {
+  displayHeader: string = 'Lealtad';
+  displayText: string =
+    'Es la fidelidad y respeto hacia el cuidado del buen nombre de Cultura Segura y quienes la conforman, el respeto a los acuerdos, las decisiones, el buen uso de las instalaciones, los equipos tecnológicos, la propiedad intelectual y confidencialidad en información de nuestros clientes. HACE ALUSION A LA HONESTIDAD.';
+
+  updateText(header: string, text: string): void {
+    this.displayHeader = header;
+    this.displayText = text;
+  }
+}
